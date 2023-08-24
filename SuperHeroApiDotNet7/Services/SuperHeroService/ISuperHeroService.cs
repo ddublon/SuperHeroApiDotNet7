c45bc15abf7a0afbb23bbf67b233d8abfcbeebc4
ddublon <d.dublon@alphaomega-eng.com>
2023-08-24 10:00:00 +0300

    Add project files.

---
+++ SuperHeroApiDotNet7/Services/SuperHeroService/ISuperHeroService.cs
@@ -0,0 +1,19 @@
+﻿using SuperHeroApiDotNet7.Models;
+
+
+namespace SuperHeroApiDotNet7.Services.SuperHeroService
+{
+    public interface ISuperHeroService
+    {
+        List<SuperHero> GetAllHeroes();
+        SuperHero? GetSingleHero(int id);
+
+        List<SuperHero> AddHero(SuperHero hero);
+
+        List<SuperHero>? UpdateHero(int id,SuperHero request);
+
+        List<SuperHero>? DeleteHero(int id);
+
+
+    }
+}
